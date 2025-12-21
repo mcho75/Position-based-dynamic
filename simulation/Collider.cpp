8042eef87ead79581bdc9c17a516784f18f1cf39
Marie <116738745+mcho75@users.noreply.github.com>
2025-12-21 14:48:05 +0100

    Clean code

--- simulation/Collider.cpp
+++ simulation/Collider.cpp
@@ -1,25 +1,29 @@
+#include <cmath>
 #include "Collider.h"
-#include "math.h"
 #include "widgets/Viewport.h"
 
 StaticConstraint* SphereCollider::checkContact(Particle& particle) {
-    double norm = sqrt((particle.position[0]- _position[0])*(particle.position[0]- _position[0]) + particle.position[1]- _position[1])*(particle.position[1]- _position[1]);
-    Vec2 nc ((particle.position-_position)/norm);
+    double norm = (particle.position[0] - _position[0]) * (particle.position[0] - _position[0])
+                + (particle.position[1] - _position[1]) * (particle.position[1] - _position[1]);
+    norm = sqrt(norm);
+    Vec2 nc = (particle.position - _position) / norm;
     if (norm - _radius < 0) {
-        return new StaticConstraint{particle, particle.position-(norm - _radius)*nc, nc};
+        return new StaticConstraint{particle, particle.position - (norm - _radius) * nc, nc};
     }
+    return nullptr;
 }
 
 StaticConstraint* PlanCollider::checkContact(Particle& particle) {
-    double a = (_end[1]- _start[1])/(_end[0]-_start[0]);
-    Vec2 pc((_end[0]+_start[0])/2, (_end[1]+_start[1])/2);
+    double a = (_end[1] -  _start[1]) / (_end[0] - _start[0]);
+    Vec2 pc = (_end + _start) / 2;
     double x = particle.position[0];
     double y = particle.position[1];
     Vec2 n(a, -1);
     double ri = particle.radius;
-    if (n[0]*(x-pc[0])+ n[1]*n[1]*(y-pc[1]) - ri < 0){ // result of the check
+    if (n[0] * (x - pc[0]) + n[1] * n[1] * (y - pc[1]) - ri < 0){
         return new StaticConstraint{particle, pc, n};
     }
+    return nullptr;
 }
 
 void SphereCollider::drawCollider(QPainter& painter, Viewport* viewport) {
